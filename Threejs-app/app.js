4e59f348c6a733363df9efc796c8a59a08d6cd86
Soltan <oskemen_ithub@astanahub.com>
2023-06-23 16:32:35 +0600

    Website files

--- Threejs-app/app.js
+++ Threejs-app/app.js
@@ -3,7 +3,14 @@ let container;
 let camera;
 let renderer;
 let scene;
-let house;
+let currentObject;
+let isMouseDown = false;
+let initialMouseX = 0;
+let initialMouseY = 0;
+let targetRotationX = 0;
+let targetRotationY = 0;
+const windowHalfX = window.innerWidth / 2;
+const windowHalfY = window.innerHeight / 2;
 
 function init() {
   container = document.querySelector(".scene");
@@ -35,60 +42,132 @@ function init() {
   container.appendChild(renderer.domElement);
 
   // Load Model
-  let loader = new THREE.GLTFLoader();
-  loader.load("./3D/gulim+++.glb", function(gltf) {
-    scene.add(gltf.scene);
-    house = gltf.scene.children[0];
+  const objectSelector = document.getElementById("objectSelector");
+
+  function loadObject(selectedObject) {
+    const objectPath = `./3D/${selectedObject}.glb`;
+    let loader = new THREE.GLTFLoader();
+    loader.load(objectPath, function (gltf) {
+      if (currentObject) {
+        scene.remove(currentObject);
+      }
   
-    // Adjust the scale of the object
-    house.scale.set(5, 5, 5); // Adjust the scale values as needed
+      currentObject = gltf.scene;
+      scene.add(currentObject);
   
-    animate();
+      // Calculate the initial scale and position of the object
+      const boundingBox = new THREE.Box3().setFromObject(currentObject);
+      const center = boundingBox.getCenter(new THREE.Vector3());
+      const size = boundingBox.getSize(new THREE.Vector3());
+  
+      const maxDimension = Math.max(size.x, size.y, size.z);
+      const desiredScale = container.clientHeight / (20 * maxDimension); // Adjust the scale based on the container height
+      const desiredPosition = new THREE.Vector3(0, 0, -maxDimension * desiredScale * 0.5); // Set the position to center the object
+  
+      currentObject.scale.set(desiredScale, desiredScale, desiredScale);
+      currentObject.position.copy(desiredPosition);
+  
+      animate();
+    });
+  }
+  
+
+  objectSelector.addEventListener("change", function (event) {
+    const selectedObject = event.target.value;
+    loadObject(selectedObject);
   });
-  
+
+  const initialSelectedObject = objectSelector.value;
+  loadObject(initialSelectedObject);
+
+  document.addEventListener("mousedown", onMouseDown);
+  document.addEventListener("mouseup", onMouseUp);
+  document.addEventListener("mousemove", onMouseMove);
+}
+
+function onMouseDown(event) {
+  isMouseDown = true;
+  initialMouseX = event.clientX;
+  initialMouseY = event.clientY;
+}
+
+function onMouseUp() {
+  isMouseDown = false;
+}
+
+function onMouseMove(event) {
+  if (!isMouseDown) return;
+
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+
+  const deltaX = mouseX - initialMouseX;
+  const deltaY = mouseY - initialMouseY;
+
+  targetRotationY += deltaX * 0.01;
+  targetRotationX += deltaY * 0.01;
+
+  initialMouseX = mouseX;
+  initialMouseY = mouseY;
 }
 
 function animate() {
   requestAnimationFrame(animate);
-  
-  // Modify the rotation values for each axis
-  house.rotation.x += 0.000; // Rotate around the x-axis
-  house.rotation.y += 0.005; // Rotate around the y-axis
-  house.rotation.z += 0.0015; // Rotate around the z-axis
+
+  // Rotate the camera around the object
+  const radius = 70;
+  const angle = targetRotationY * Math.PI / 180; // Convert to radians
+  camera.position.x = Math.sin(angle) * radius;
+  camera.position.z = Math.cos(angle) * radius;
+  camera.lookAt(scene.position);
+
+  // Rotate the object based on mouse movements
+  currentObject.rotation.y += 0.1 * (targetRotationY - currentObject.rotation.y);
+  currentObject.rotation.x += 0.1 * (targetRotationX - currentObject.rotation.x);
+
+  // Calculate the maximum allowed object scale based on the canvas size
+  const maxCanvasSize = Math.min(container.clientWidth, container.clientHeight);
+  const maxAllowedScale = maxCanvasSize / 4; // Adjust the value as desired
+
+  // Scale the object while keeping it within the canvas limits
+  const boundingBox = new THREE.Box3().setFromObject(currentObject);
+  const objectSize = boundingBox.getSize(new THREE.Vector3());
+  const maxObjectSize = Math.max(objectSize.x, objectSize.y, objectSize.z);
+
+  if (maxObjectSize > maxAllowedScale) {
+    const scaleDownFactor = maxAllowedScale / maxObjectSize;
+    currentObject.scale.set(scaleDownFactor, scaleDownFactor, scaleDownFactor);
+  }
 
   renderer.render(scene, camera);
 }
 
 
+
+
+
+
 init();
 
 function onWindowResize() {
-  // Calculate the new aspect ratio
   const aspect = container.clientWidth / container.clientHeight;
 
-  // Update the camera
   camera.aspect = aspect;
   camera.updateProjectionMatrix();
 
-  // Update the renderer size
   renderer.setSize(container.clientWidth, container.clientHeight);
 
-  // Adjust the size and position of the object
-  const boundingBox = new THREE.Box3().setFromObject(house);
-  const center = boundingBox.getCenter(new THREE.Vector3());
+  if (currentObject) {
+    const boundingBox = new THREE.Box3().setFromObject(currentObject);
+    const center = boundingBox.getCenter(new THREE.Vector3());
+    const size = boundingBox.getSize(new THREE.Vector3());
 
-  // Adjust the object's position to the center of the screen
-  house.position.x = -center.x;
-  house.position.y = -center.y;
+    const maxDimension = Math.max(size.x, size.y, size.z);
+    const scale = 5 / maxDimension;
 
-  // Calculate the scale factor based on the aspect ratio
-  const scaleFactor = Math.max(
-    boundingBox.max.x - boundingBox.min.x,
-    boundingBox.max.y - boundingBox.min.y
-  );
-
-  // Adjust the object's scale to cover most of the website
-  house.scale.set(aspect * scaleFactor, aspect * scaleFactor, aspect * scaleFactor);
+    currentObject.position.set(-center.x * scale, -center.y * scale, -center.z * scale);
+    currentObject.scale.set(scale, scale, scale);
+  }
 }
 
 window.addEventListener("resize", onWindowResize);
